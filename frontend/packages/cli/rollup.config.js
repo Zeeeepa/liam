199ed19901d08c2649cb0efbe2c5a0fafbf52f9b
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-11-21 16:16:05 +0900

    index.htmlがみつからん

--- frontend/packages/cli/rollup.config.js
+++ frontend/packages/cli/rollup.config.js
@@ -1,14 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/cli/index.ts', // ここにCLIのエントリーポイントのパスを指定
+  input: 'bin/cli.ts',
   output: {
-    file: 'dist-cli/bin/cli.js', // 出力ファイルのパス
-    format: 'cjs', // CommonJS形式で出力
+    file: 'dist-cli/bin/cli.js',
+    format: 'esm',  // ECMAScript モジュール形式で出力
+    // banner: '#!/usr/bin/env node',
   },
   plugins: [
+    resolve({
+      preferBuiltins: true,
+      extensions: ['.mjs', '.js', '.json', '.node', '.ts']
+    }),
+    commonjs(),
     typescript({
-      tsconfig: './tsconfig.node.json' // TypeScript設定ファイルのパス
+        tsconfig: './tsconfig.node.json' // TypeScript設定ファイルのパス
     })
-  ]
+  ],
+  external: ['commander', 'vite']
 };
