a28e843d70ac1c5107f73319bb403f575bd09eed
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2024-12-27 15:18:51 +0900

    [ci skip] PoC: Offline ERD

--- frontend/packages/cli/vite.config.ts
+++ frontend/packages/cli/vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
         format: 'iife',
       },
     },
+    modulePreload: false,
   },
   plugins: [react(), tsconfigPaths(), offlinePlugin(), setEnvPlugin()],
   test: {
